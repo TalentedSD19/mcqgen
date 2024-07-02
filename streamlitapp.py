840bfb6afdee936c77792575c33754786cd594b5
TalentedSD19 <talentedsd19@gmail.com>
2024-07-03 03:08:44 +0530

    updated

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,7 +10,7 @@ from src.mcqgenerator.mcqgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
 
-#loading json file
+#loading json file from root
 
 with open('Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
